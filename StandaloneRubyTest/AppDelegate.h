bee81190cffb137bad1a1768de747f9214b613eb
Eralp Karaduman <eralpkaraduman@gmail.com>
2013-08-01 15:12:44 +0300

    Initial Commit

---
+++ StandaloneRubyTest/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  StandaloneRubyTest
+//
+//  Created by eralp on 8/1/13.
+//  Copyright (c) 2013 SuperDamage. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ StandaloneRubyTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  StandaloneRubyTest
+//
+//  Created by eralp on 8/1/13.
+//  Copyright (c) 2013 SuperDamage. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
